1d8977b7d84aba09e4633f66a25e7b426d0da817
Augusto Seabra <augusto.seabra00@gmail.com>
2025-09-22 11:07:02 -0300

    feat: add header comment to clarify the purpose of the web scraping script

--- scripts/scraping_codigo_habilidades.py
+++ scripts/scraping_codigo_habilidades.py
@@ -1,3 +1,5 @@
+# Esse arquivo pode ser usado para criar um webscraping afim de filtrar códigos diferentes do 4º ano (que é o foco do projeto atual)
+
 matematica_urls = ['https://basenacionalcomum.mec.gov.br/abase/#fundamental/matematica-no-ensino-fundamental-anos-iniciais-unidades-tematicas-objetos-de-conhecimento-e-habilidades', 'https://basenacionalcomum.mec.gov.br/abase/#fundamental/matematica-no-ensino-fundamental-anos-finais-unidades-tematicas-objetos-de-conhecimento-e-habilidades']
 
 ciencia_urls = ['https://basenacionalcomum.mec.gov.br/abase/#fundamental/ciencias-no-ensino-fundamental-anos-iniciais-unidades-tematicas-objetos-de-conhecimento-e-habilidades', 'https://basenacionalcomum.mec.gov.br/abase/#fundamental/ciencias-no-ensino-fundamental-anos-finais-unidades-tematicas-objetos-de-conhecimento-e-habilidades']
